3b5a99726125863bc31fdf23018390e9f88b47b1
chaddwick25 <chaddwick25@gmail.com>
2017-09-08 23:11:16 -0400

    First Commit

---
+++ template/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  // $(".blue-box").fadeTo(2000 , 0.2);
+  $(".green-box").fadeTo(3000 , 0.5);
+  $(".red-box").fadeTo(1000 , 0.80);
+  $(".blue-box").slideUp(1000);
+  $(".blue-box").slideDown(1000);
+  $("p").hide();
+  $("p").slideDown(1000);
+
+
+
+;
+
+  // $(".red-box").fadeTo(1000 , 0.80);
+
+});
